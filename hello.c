e00cd905830153376f76a17f1ed8c8aff1d1f724
hengxin <hfwei@nju.edu.cn>
2022-09-23 23:30:21 +0800

    +hello.c, +guess.c

---
+++ hello.c
@@ -0,0 +1,32 @@
+// This is a single line comment.
+// (1) comments
+// Created by hengxin on 9/23/22.
+//
+
+// stdio.h: standard input/output
+// (2) preprocessor directive
+#include <stdio.h>
+
+/*
+ * This is a multi-line comment.
+ * This is a multi-line comment.
+ */
+// (3) main function
+/*
+ * Any executable C program must contain one and only one main function.
+ * main: accept no arguments;
+ * int: integer; return integer
+ */
+int main() {
+  // (4) statement
+  /*
+   * printf: print + f: format
+   * "Hello, World!\n":
+   * (1) \n: newline
+   * (2) "Hello, World!\n": C string
+   */
+  printf("Hello World!\n");
+
+  // (4) statement
+  return 0;
+}
