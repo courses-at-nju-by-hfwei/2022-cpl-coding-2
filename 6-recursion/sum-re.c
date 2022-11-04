99eb12816919ed2b2c4c864f5c212b9f199a90c0
Hengfeng Wei <hengxin>
2022-11-04 18:08:29 +0800

    +6-recursion

--- 6-recursion/sum-re.c
+++ 6-recursion/sum-re.c
@@ -10,8 +10,23 @@
 
 #include <stdio.h>
 
+int Sum(int nums[], int len);
+
 int main() {
 	int numbers[] = {1, 2, 3, 4, 5};
 
+  int sum = Sum(numbers, sizeof numbers / sizeof numbers[0]);
+
+  printf("sum = %d\n", sum);
+
 	return 0;
+}
+
+int Sum(int nums[], int len) {
+  if (len == 0) {
+    return 0;
+  }
+
+  int partial_sum = Sum(nums, len - 1);
+  return partial_sum + nums[len - 1];
 }
