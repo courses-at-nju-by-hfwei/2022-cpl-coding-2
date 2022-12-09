afbc8261a10849cc435d6a69c2afb049a0b99e5f
Hengfeng Wei <hengxin>
2022-12-09 17:59:42 +0800

    11-struct: +code in class

--- 10-double-pointers/sort.c
+++ 10-double-pointers/sort.c
@@ -14,18 +14,25 @@
 void PrintInts(const int *integers, int len);
 void PrintStrs(const char *str[], int len);
 
+int CompareInts(const void *left, const void *right);
+int CompareStrs(const void *left, const void *right);
+
 int main() {
   // sort an array of integers
   int integers[] = {-2, 99, 0, -743, 2, INT_MIN, 4};
   int size_of_integers = sizeof integers / sizeof *integers;
 
   /**
+   * q: quick
    * qsort: https://en.cppreference.com/w/c/algorithm/qsort
    * void qsort( void *ptr, size_t count, size_t size,
             int (*comp)(const void *, const void *) );
    */
-  // sort ints here
+  int (*comp)(const void*, const void*) = CompareInts;
 
+  // sort ints here
+  qsort(integers, size_of_integers,
+        sizeof *integers, comp);
   PrintInts(integers, size_of_integers);
 
   // Sorting an array of strings
@@ -44,7 +51,9 @@ int main() {
   int size_of_names = sizeof names / sizeof *names;
 
   // sort musicians here
-
+  comp = CompareStrs;
+  qsort(names, size_of_names,
+        sizeof *names, comp);
   PrintStrs(names, size_of_names);
 }
 
@@ -62,4 +71,29 @@ void PrintStrs(const char *str[], int len) {
     printf("%s\n", str[i]);
   }
   printf("\n");
+}
+
+// actual argument: const int *left
+int CompareInts(const void *left, const void *right) {
+  int left_int = *(const int*) left;
+  int right_int = *(const int *) right;
+
+  if (left_int < right_int) {
+    return -1;
+  }
+
+  if (left_int > right_int) {
+    return 1;
+  }
+
+  return 0;
+  // return left_int - right_int;
+}
+
+// actual argument: char *const *left
+int CompareStrs(const void *left, const void *right) {
+  char *const *pp1 = left;
+  char *const *pp2 = right;
+
+  return strcmp(*pp1, *pp2); // const char *
 }
--- 11-struct/musician.c
+++ 11-struct/musician.c
@@ -2,7 +2,78 @@
 // Created by hfwei on 2022/12/8.
 //
 
+#include <stdio.h>
+#include <string.h>
+
+typedef enum gender {
+  MALE, FEMALE,
+} Gender;
+
+typedef struct score {
+  int c_score; // 4
+  int java_score; // 4
+  int python_score; // 4 (+4)
+} Score;
+
+// tag
+typedef struct musician {
+  char *name; // 8
+  Gender gender; // 1 (+ 7)
+  char *album; // 8
+
+  Score score;
+} Musician;
+
+void PrintMusician(const Musician m);
+
 int main() {
+  printf("sizeof Musician : %zu\n", sizeof(Musician));
+
+  Musician luo = {
+      .name = "luo dayou",
+      .gender = MALE,
+      .album = "ZhiHuZheYe",
+      .score = {10, 20, 30},
+      // .score.c_score = 10,
+      // .score.java_score = 20,
+      // .score.python_score = 30,
+  };
+
+  Musician cui = {
+      .name = "cui jian",
+      .gender = MALE,
+      .album = "XinChangZhengLuShangDeYaoGun",
+      .score.c_score = 10,
+      .score.java_score = 20,
+      .score.python_score = 30,
+  };
+
+  char album[] = "YiKeBuKenMeiSuDeXin";
+
+  Musician zhang = {
+      .name = "zhang chu",
+      .gender = MALE,
+      .album = album,
+      .score.c_score = 10,
+      .score.java_score = 20,
+      .score.python_score = 30,
+  };
+
+  Musician guo = zhang;
+  guo.name = "guo";
+  strcpy(guo.album, "YiKeJiuMeiSuDeXin");
+  PrintMusician(guo);
+  PrintMusician(zhang);
 
   return 0;
+}
+
+void PrintMusician(const Musician m) {
+  printf("\n%s\t%d\t%s\t%d\t%d\t%d\n",
+         m.name,
+         m.gender,
+         m.album,
+         m.score.c_score,
+         m.score.java_score,
+         m.score.python_score);
 }
